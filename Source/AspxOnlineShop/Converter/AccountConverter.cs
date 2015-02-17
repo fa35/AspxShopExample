332069eea3483b739094362548964ed72093731f
luxfinis <luxfinis@ruggedinbox.com>
2015-02-17 07:26:25 +0100

    first commit

---
+++ Source/AspxOnlineShop/Converter/AccountConverter.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Mail;
+using AspxOnlineShop.Database;
+using AspxOnlineShop.Models;
+
+namespace AspxOnlineShop.Converter
+{
+    public class AccountConverter
+    {
+        public Nutzer ConvertToAccount(Registration reg)
+        {
+            var nutzer = new Nutzer();
+
+
+            if(string.IsNullOrWhiteSpace(reg.FirstName))
+                return null;
+            nutzer.Vorname = reg.FirstName;
+
+
+            if (string.IsNullOrWhiteSpace(reg.LastName))
+                return null;
+            nutzer.Name = reg.LastName;
+
+
+            if (string.IsNullOrWhiteSpace(reg.MailAddress) || !reg.MailAddress.Contains("@"))
+                return null;
+            nutzer.EMail = reg.MailAddress;
+
+            var mma = new MailAddress(reg.MailAddress);
+
+
+            if (string.IsNullOrWhiteSpace(reg.Password))
+                return null;
+            nutzer.Passwort = reg.Password;
+
+
+            if(string.IsNullOrWhiteSpace(reg.Street))
+                return null;
+            nutzer.Strasse = reg.Street;
+
+
+            if (string.IsNullOrWhiteSpace(reg.HouseNr))
+                return null;
+            nutzer.HausNr = reg.HouseNr;
+
+
+            var dig = int.Parse(reg.PostalCode);
+
+            if (dig == null || dig < 10000)
+                return null;
+            nutzer.PLZ = dig;
+
+
+            if (string.IsNullOrWhiteSpace(reg.Place))
+                return null;
+            nutzer.Ort = reg.Place;
+
+
+            return nutzer;
+        }
+
+    }
+}
---
+++ Source/AspxOnlineShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspxOnlineShop.MvcApplication" Language="C#" %>
